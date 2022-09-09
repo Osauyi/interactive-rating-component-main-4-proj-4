91152ebfcdf3e58e797a75ebe2c9355e9d1644ea
Osauyi <>
2022-09-09 10:51:01 +0100

    change button colors when clicked

--- index.js
+++ index.js
@@ -1,34 +1,54 @@
-
 let clicked = ""
 
-function buttonClicked(){
-document.querySelector(".item")
+const clickedbutton = document.querySelectorAll(".item")
+
+for(item in clickedbutton) {
+	clickedbutton[item].onclick = function(){
+        var changeDaColor = document.querySelectorAll(".whenclicked")[0];
+    	if(this.className == "item") {
+            if( changeDaColor ) changeDaColor.className = "item";
+            this.className = "whenclicked";
+        }
+    }
+  
+}
+
+document.querySelector("#firstvalue").addEventListener("click", buttonClicked)
+document.querySelector("#secondvalue").addEventListener("click", buttonClicked2)
+document.querySelector("#thirdvalue").addEventListener("click", buttonClicked3)
+document.querySelector("#fourthvalue").addEventListener("click", buttonClicked4)
+document.querySelector("#fifthvalue").addEventListener("click", buttonClicked5)
+
+
+  function buttonClicked(event) {
+  document.querySelector("#firstvalue")
   clicked = 1
+
 }
 
-function buttonClicked2(){
-document.querySelector(".item")
+function buttonClicked2(event) {
+  document.querySelector("#secondvalue")
   clicked = 2
+  
 }
 
-function buttonClicked3(){
-document.querySelector(".item")
+function buttonClicked3(event) {
+  document.querySelector("#thirdvalue")
   clicked = 3
 }
 
-function buttonClicked4(){
-document.querySelector(".item")
+function buttonClicked4(event) {
+  document.querySelector("#fourthvalue")
   clicked = 4
-
 }
 
-function buttonClicked5(){
-document.querySelector(".item")
+function buttonClicked5(event) {
+  document.querySelector("#fifthvalue")
   clicked = 5
 }
 
 
-function nextPage(){
+function nextPage() {
   document.querySelector(".button")
   location.href = "greeting.html";
   save()
@@ -36,11 +56,11 @@ function nextPage(){
 
 function save() {
   var getInput = clicked;
-  localStorage.setItem("drive",getInput);
+  localStorage.setItem("drive", getInput);
 }
 
 
-function thankyou(){
-let collect = localStorage.getItem("drive")
-document.querySelector(".numberSelected").innerHTML = collect
+function thankyou() {
+  let collect = localStorage.getItem("drive")
+  document.querySelector(".numberSelected").innerHTML = collect
 }
